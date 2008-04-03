3a53d527536ff20b4b3a2e16e4538c1ee8d1d370
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2008-04-03 13:02:36 +0000

    This works in 1_6_X too

--- interface/MuonSelector.h
+++ interface/MuonSelector.h
@@ -2,63 +2,75 @@
 #define PhysicsTools_PatUtils_MuonSelector_h
 
 /**
-    \class MuonSelector MuonSelector.h "PhysicsTools/PatUtils/MuonSelector.h"
+    \class pat::MuonSelector MuonSelector.h "PhysicsTools/PatUtils/MuonSelector.h"
     \brief Selects good muons
 
-    The muon selector returns a flag (passed=0) based on one of the possible
-    selections: reconstruction-based (global muons) or muId based (various algorithms) 
-    or custom (user-defined set of cuts). This is driven by the configuration parameters.
+    The muon selector returns a flag (see pat::ParticleStatus) based on one of the possible
+    selections: reconstruction-based (global muons) or muId based (various algorithms),
+    or custom (user-defined set of cuts). This is driven by the configuration parameters 
+    (see the PATMuonCleaner documentation for configuration details).
+    
+    The parameters are passed to the selector through an MuonSelection struct.
+    (An adapter exists for use in CMSSW: reco::modules::ParameterAdapter< pat::MuonSelector >.)
 
-    PSet selection = {
-      string type = "none | globalMuons | muId  | custom" // muId not implemented yet
-      [ // If custom, give cut values
-        double dPbyPmax = ...
-        double chi2max  = ...
-        int    nHitsMin = ...
-      ]
-
-    }
-
-    \author F. Ronga (ETH Zurich)
-    \version $Id: MuonSelector.h,v 1.4 2008/02/04 14:20:55 fronga Exp $
+    \author F.J. Ronga (ETH Zurich)
+    \version $Id: MuonSelector.h,v 1.5 2008/04/02 15:42:24 zeidler Exp $
 */
 
 #include <string>
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include "DataFormats/MuonReco/interface/Muon.h"
+#include "RecoMuon/MuonIdentification/interface/IdGlobalFunctions.h"
+
+#include "PhysicsTools/PatUtils/interface/ParticleCode.h"
+
 
 namespace pat {
 
-  enum MuonType { GOOD = 0, BAD };
+  /// Structure defining the muon selection
+  struct MuonSelection {
+    std::string selectionType; ///< Choose selection type (see PATMuonCleaner)
+    
+    /// @name Cuts for "custom" selection type:
+    //@{
+    double dPbyPmax;
+    double chi2max;
+    int    nHitsMin;
+    //@}
+	
+	/// @name Option for "muId" selection type:
+    //@{
+    muonid::SelectionType flag;
+    //@}
+	
+  };
+
 
   class MuonSelector {
 
   public:
     
-    MuonSelector( const edm::ParameterSet& config );
+    MuonSelector( const MuonSelection& cfg ) : config_( cfg ) {}
     ~MuonSelector() {}
 
     /// Returns 0 if muon matches criteria, a flag otherwise.
     /// Criteria depend on the selector's configuration.
-    const unsigned int 
+    const pat::ParticleStatus
     filter( const unsigned int& index,
-            const edm::View<reco::Muon>&   muons ) const;
+            const edm::View<reco::Muon>& muons ) const;
     
   private:
-
-    edm::ParameterSet selectionCfg_; 
-    std::string       selectionType_;
+    
+    MuonSelection config_;
 
     /// Full-fledged selection based on SusyAnalyser
-    const unsigned int  
+    const pat::ParticleStatus
     customSelection_( const unsigned int& index,
                       const edm::View<reco::Muon>& muons  ) const;
+	const pat::ParticleStatus
+    muIdSelection_( const unsigned int& index,
+                      const edm::View<reco::Muon>& muons  ) const;
     
-    // Custom selection cuts
-    double dPbyPmax_, chi2max_;
-    int    nHitsMin_;
-
 
   }; // class
 } // namespace
